49cb1ef8faa1470cabd7d0a2b99abd617f13a5d2
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2025-04-15 21:54:49 -0300

    add funçao para exclusao e ediçao dos arrays da tabela dadosLista

--- main.js
+++ main.js
@@ -27,6 +27,38 @@ function salvarUser()
     {
         dadosLista.push(nomeUser);
         console.log(dadosLista);
+        criaLista();
         document.getElementById ('nomeUser').value = "";
     }
+    else
+    {
+        alert("Favor inserir um nome para cadastro");
+    }
+}
+
+//funçao para preencher a lista de cadastro tabela
+
+function criaLista()
+{
+    let tabela = "<tr><td>Nome</td><th>Ações</th></tr>";
+
+    for(let i = 0; i <= (dadosLista.length -1); i++)
+    {
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><td><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;       
+    }
+}
+
+//funcao para exluir nome da lista 
+
+function excluir(i)
+{
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
+}
+
+function editar(i)
+{
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);   
 }
